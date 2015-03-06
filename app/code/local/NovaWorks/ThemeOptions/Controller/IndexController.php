e37157b038d7bf46c3217fe21d1b33512a01f0cd
Bryson <brysonwf@gmail.com>
2015-03-06 13:18:15 -0500

    removing junk from plugin

--- app/code/local/NovaWorks/ThemeOptions/Controller/IndexController.php
+++
@@ -1,9 +0,0 @@
-<?php
-class NovaWorks_ThemeOptions_IndexController extends Mage_Core_Controller_Front_Action
-{
-    public function indexAction()
-    {			
-		$this->loadLayout();     
-		$this->renderLayout();
-    }
-}
